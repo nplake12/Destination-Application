407eb59edd0102fb737a2acb27c83c1d9a867417
Eric Chavarria <eachavarria@bsu.edu>
2017-10-15 15:10:22 -0400

    Add Place class and Make PlaceParser return a List<Place>
    Add test cases for the first name, rating, and address of the first place in the List<Place>

--- src/main/java/PlaceParser.java
+++ src/main/java/PlaceParser.java
@@ -2,6 +2,7 @@ import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import jdk.nashorn.internal.parser.JSONParser;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -9,9 +10,13 @@ import java.io.InputStreamReader;
 import java.io.Reader;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.LinkedList;
+import java.util.List;
+
 public class PlaceParser {
 
-    public String parse(String location) throws IOException{
+    public List<Place> parse(String location) throws IOException {
+        List<Place> places = new LinkedList<Place>();
         String geocodingSearch = "https://maps.googleapis.com/maps/api/geocode/json?address=" + location + "&key=AIzaSyDZ4V52UN8Bpw5Lwtwbk4AfBp45djbmP-c";
         URL url = new URL(geocodingSearch);
         URLConnection connection = url.openConnection();
@@ -27,6 +32,24 @@ public class PlaceParser {
                 .get("geometry")
                 .getAsJsonObject();
         JsonObject locationCoordinates = geometry.get("location").getAsJsonObject();
-        return locationCoordinates.get("lat") + ","  + locationCoordinates.get("lng");
+        String coordinates = locationCoordinates.get("lat") + "," + locationCoordinates.get("lng");
+        String googlePlacesSearch = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + coordinates + "&radius=100&type=restaurant&key=AIzaSyAlmnrNiNrVybKz8JbYjOzuxZrGVRI9-Gg";
+        URL placesUrl = new URL(googlePlacesSearch);
+        URLConnection placesConnection = placesUrl.openConnection();
+        InputStream placesInputStream = placesConnection.getInputStream();
+        Reader placesReader = new InputStreamReader(placesInputStream);
+        JsonElement placesRootElement = parser.parse(placesReader);
+        JsonObject placesRootObject = placesRootElement.getAsJsonObject();
+        JsonArray placesResults = placesRootObject.get("results").getAsJsonArray();
+        for (JsonElement place : placesResults) {
+            Place googlePlace = new Place.Builder()
+                    .setName(place.getAsJsonObject().get("name").getAsString())
+                    .setRating(place.getAsJsonObject().get("rating").getAsString())
+                    .setAddress(place.getAsJsonObject().get("vicinity").getAsString())
+                    .build();
+            places.add(googlePlace);
         }
+        return places;
     }
+}
+
--- src/test/java/PlaceParserTest.java
+++ src/test/java/PlaceParserTest.java
@@ -2,13 +2,35 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.util.List;
 
 public class PlaceParserTest {
 
+//    @Test
+//    public void testParse() throws IOException{
+//        PlaceParser parser = new PlaceParser();
+//        String coordinates = parser.parse("Muncie,IN");
+//        Assert.assertEquals("40.1933767,-85.3863599", coordinates);
+//    }
+
     @Test
-    public void testParse() throws IOException{
+    public void testParse_firstName() throws IOException{
         PlaceParser parser = new PlaceParser();
-        String coordinates = parser.parse("Muncie,IN");
-        Assert.assertEquals("40.1933767,-85.3863599", coordinates);
+        List<Place> places = parser.parse("Muncie,IN");
+        Assert.assertEquals("The Downtown Farm Stand", places.get(0).getName());
+    }
+
+    @Test
+    public void testParse_firstRating() throws IOException{
+        PlaceParser parser = new PlaceParser();
+        List<Place> places = parser.parse("Muncie,IN");
+        Assert.assertEquals("4.6", places.get(0).getRating());
+    }
+
+    @Test
+    public void testParse_firstAddress() throws IOException{
+        PlaceParser parser = new PlaceParser();
+        List<Place> places = parser.parse("Muncie,IN");
+        Assert.assertEquals("125 East Main Street, Muncie", places.get(0).getAddress());
     }
 }
