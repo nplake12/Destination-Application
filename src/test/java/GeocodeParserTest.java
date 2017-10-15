67eb788d37f5c8101776599ffa01b05ae719a6c4
Eric Chavarria <eachavarria@bsu.edu>
2017-10-15 15:25:06 -0400

    Add GeocodeParser class and Add GeocodeParserTest class with a unit test
    Refactor PlaceParser to implement GeocodeParser and it's responsibilities

---
+++ src/test/java/GeocodeParserTest.java
@@ -0,0 +1,14 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.IOException;
+
+public class GeocodeParserTest {
+
+    @Test
+        public void testParse() throws IOException{
+        GeocodeParser parser = new GeocodeParser();
+        String coordinates = parser.parse("Muncie,IN");
+        Assert.assertEquals("40.1933767,-85.3863599", coordinates);
+    }
+}
